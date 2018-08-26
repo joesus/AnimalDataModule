4a06c2967c3df267b260dfd2b6ebe290f34f9c44
joesus <joesusnick@gmail.com>
2018-08-26 15:21:08 -0600

    Creates project

---
+++ AnimalData/AnimalData/AnimalData.h
@@ -0,0 +1,19 @@
+//
+//  AnimalData.h
+//  AnimalData
+//
+//  Created by Joe Susnick on 8/26/18.
+//  Copyright © 2018 Joe Susnick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimalData.
+FOUNDATION_EXPORT double AnimalDataVersionNumber;
+
+//! Project version string for AnimalData.
+FOUNDATION_EXPORT const unsigned char AnimalDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimalData/PublicHeader.h>
+
+
